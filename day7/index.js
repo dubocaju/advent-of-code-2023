9ecdd55c8610bd30a22f228350c3dcca622072aa
Julien Dubocage <dubocage.julien68@gmail.com>
2023-12-15 22:08:17 +0100

    fin partie 1 jour 7

--- day7/index.js
+++ day7/index.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 const data = fs.readFileSync(process.argv[2], 'utf-8').split(/\r?\n/);
+const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].reverse();
 const game = [];
 
 data.forEach(line => {
     const split = line.split(' ');
     game.push({"hand": split[0], "bet": split[1]});
- });
+});
 
 const countCardOccurences = hand => {
     const occurences = {};
-    hand.forEach(card => {
+    hand.split('').forEach(card => {
         if (occurences[card] == undefined) {
             occurences[card] = 1;
         }
@@ -50,20 +51,29 @@ const computeHandScore = hand => {
     return 1;
 }
 
-const compareCards = (firstCard, secondCard) => {
-    if (firstCard == secondCard) {
-        return 0;
-    }
-    //TODO
-}
+const compareCards = (firstCard, secondCard) => cards.indexOf(firstCard) - cards.indexOf(secondCard);
 
 const compareHands = (firstHand, secondHand) => {
     const firstHandScore = computeHandScore(firstHand);
     const secondHandScore = computeHandScore(secondHand);
-
+    
     if (firstHandScore != secondHandScore) {
         return firstHandScore - secondHandScore;
     }
-
-    //TODO cas d'égalité (appeler compareHands)
+    
+    for (let i = 0; i < firstHand.length; i++) {
+        const cardsComparaison = compareCards(firstHand[i], secondHand[i]);
+        if (cardsComparaison !== 0) {
+            return cardsComparaison;
+        }
+    }
 }
+
+const gameResult = game
+.sort((a,b) => compareHands(a.hand, b.hand))
+.map((value, index) => { return { ...value, "rank": index + 1 } });
+
+const totalWinnings = gameResult.reduce((acc, value) => acc += value.bet * value.rank, 0);
+
+//console.log(gameResult);
+console.log("Total winnings : " + totalWinnings);
